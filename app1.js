91d7e0cb215b17b0a35f22e2b927b845a85b39ce
Tanishk Saxena <tanishksaxena686@outlook.com>
2022-11-14 04:15:51 +0530

    initial commit

---
+++ app1.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const { config } = require("dotenv");
+config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT1 || 5555;
+
+app.listen(port, ()=>{
+    console.log(`Server up and running successfully at port ${port}.`);
+})
+
+app.post('/run', (req, res)=>{
+    res.send("Hello");
+})
